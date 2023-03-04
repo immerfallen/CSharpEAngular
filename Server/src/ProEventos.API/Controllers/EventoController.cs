1aaf9c9685d54d59f7f94550a3846540f845797b
maro <maro@bwstec.com>
2023-03-04 12:02:42 -0300

    commit inicial

---
+++ Server/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public EventoController()
+        {
+
+        }
+
+        [HttpGet]
+        public Evento Get()
+        {
+            return new Evento()
+            {
+                EventoId = 1,
+                DataEvento = DateTime.Now.AddDays(2).ToString(),
+                Tema = "Selva",
+                Local = "Terra",
+                QtdPessoas = 13,
+                Lote = "qw23",
+                ImagemUrl = "google.com"
+            };
+        }
+
+    }
+}
