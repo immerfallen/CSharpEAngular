84873d0a9227a0199bcc9b3593ca30f16c51288b
maro <maro@bwstec.com>
2023-03-05 14:25:44 -0300

    Separando o back end em camadas

--- Client/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Client/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -15,18 +15,16 @@ export class EventosComponent implements OnInit {
   public showImage: boolean = true;
   private _filterString: string = "";
 
-  public get filterString(): string{
+  constructor(private http: HttpClient) { }
+
+  public get filterString(): string {
     return this._filterString;
   }
 
-  public set filterString(value: string){
+  public set filterString(value: string) {
     this._filterString = value;
     this.eventosFiltrados = this.filterString ? this.filtrarEventos(this.filterString) : this.eventos;
   }
-
-
-
-  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.getEventos();
@@ -34,21 +32,21 @@ export class EventosComponent implements OnInit {
 
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/evento').subscribe(
-      response => {this.eventos = response; this.eventosFiltrados = response},
+      response => { this.eventos = response; this.eventosFiltrados = response },
       error => console.log(error)
     );
   }
 
-  public handleClickShowImage(){
+  public handleClickShowImage() {
     this.showImage = !this.showImage
   }
 
-  public filtrarEventos(filterString: string): any{
+  public filtrarEventos(filterString: string): any {
     filterString = filterString.toLowerCase();
     return this.eventos.filter(
       (evento: any) => evento.tema.toLowerCase().indexOf(filterString) !== -1
-      || evento.local.toLowerCase().indexOf(filterString) !== -1
+        || evento.local.toLowerCase().indexOf(filterString) !== -1
     )
-      }
+  }
 
 }
