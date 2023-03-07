aed133b77ffc417c02b1d66a58d8ec71614cd18d
maro <maro@bwstec.com>
2023-03-07 19:41:05 -0300

    Implementados métodos Add, Update e Delete da classe Service

---
+++ Server/src/ProEventos.Application/EventoService.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProEventos.Application.Interfaces;
+using ProEventos.Domain.Models;
+using ProEventos.Persistence.Interfaces;
+
+namespace ProEventos.Application
+{
+    public class EventoService : IEventoService
+    {
+        private readonly IGeralPersistence _geralPersistence;
+        private readonly IEventoPersistence _eventoPersistence;
+
+        public EventoService(IGeralPersistence geralPersistence, IEventoPersistence eventoPersistence)
+        {
+            _geralPersistence = geralPersistence;
+            _eventoPersistence = eventoPersistence;
+
+        }
+        public async Task<Evento> AddEvento(Evento model)
+        {
+            try
+            {
+                _geralPersistence.Add<Evento>(model);
+                if (await _geralPersistence.SaveChangesAsync())
+                {
+                    return await _eventoPersistence.GetEventoByIdAsync(model.Id, false);
+                }
+                return null;
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+         public async Task<Evento> UpdateEvento(int eventoId, Evento model)
+        {
+            try
+            {
+                var evento = await _eventoPersistence.GetEventoByIdAsync(eventoId, false);
+                if(evento == null) return null;
+
+                model.Id = evento.Id;
+
+                _geralPersistence.Update(evento);
+                 if (await _geralPersistence.SaveChangesAsync())
+                {
+                    return await _eventoPersistence.GetEventoByIdAsync(model.Id, false);
+                }
+                return null;
+            }
+            catch (Exception ex)
+            {
+                
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<bool> DeleteEvento(int eventoId)
+        {
+            try
+            {
+                var evento = await _eventoPersistence.GetEventoByIdAsync(eventoId, false);
+                if(evento == null) throw new Exception("Evento para delete não foi encontrado");               
+
+                _geralPersistence.Delete<Evento>(evento);
+                 return await _geralPersistence.SaveChangesAsync();
+                
+            }
+            catch (Exception ex)
+            {
+                
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public Task<Evento[]> GetAllEventosAsync(bool includePalestrantes = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Evento[]> GetAllEventosByTemaAsync(string tema, bool includePalestrantes = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Evento> GetEventoByIdAsync(int eventoId, bool includePalestrantes = false)
+        {
+            throw new NotImplementedException();
+        }
+
+       
+    }
+}
--- Server/src/ProEventos.Application/Interfaces/IEventoService.cs
+++ Server/src/ProEventos.Application/Interfaces/IEventoService.cs
@@ -10,7 +10,7 @@ namespace ProEventos.Application.Interfaces
     {
         Task<Evento> AddEvento(Evento model);
 
-        Task<Evento> UpdateEvento(Evento model);
+        Task<Evento> UpdateEvento(int eventoId,Evento model);
 
         Task<bool> DeleteEvento(int eventoId);
 
