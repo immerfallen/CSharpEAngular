5efd5cf2530ba7d176e7226c82a7d316c7edb455
maro <maro@bwstec.com>
2023-03-12 15:38:45 -0300

    Corrigindo comportamento cíclico do JSON

--- Server/src/ProEventos.API/Startup.cs
+++ Server/src/ProEventos.API/Startup.cs
@@ -29,7 +29,9 @@ namespace ProEventos.API
             services.AddDbContext<ProEventosContext>(
                 context => context.UseSqlite(Configuration.GetConnectionString("Default"))
             );
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(x=>x.SerializerSettings.ReferenceLoopHandling = 
+            Newtonsoft.Json.ReferenceLoopHandling.Ignore
+            );
 
             services.AddScoped<IEventoService, EventoService>();
             services.AddScoped<IGeralPersistence, GeralPersistence>();
