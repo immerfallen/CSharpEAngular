99d13131110720a21d246149577a0858f933813f
immerfallen <marodemelo10@gmail.com>
2023-03-19 10:31:34 -0300

    Alterando compeortamento do delete para ser cascade

--- Server/src/ProEventos.Persistence/Context/ProEventosContext.cs
+++ Server/src/ProEventos.Persistence/Context/ProEventosContext.cs
@@ -6,10 +6,10 @@ namespace ProEventos.Persistence.Context
 {
     public class ProEventosContext : DbContext
     {
-        public ProEventosContext(DbContextOptions<ProEventosContext> options) 
+        public ProEventosContext(DbContextOptions<ProEventosContext> options)
         : base(options)
         {
-            
+
         }
         public DbSet<Evento> Evento { get; set; }
         public DbSet<Lote> Lote { get; set; }
@@ -17,9 +17,13 @@ namespace ProEventos.Persistence.Context
         public DbSet<RedeSocial> RedeSocial { get; set; }
         public DbSet<PalestranteEvento> PalestranteEvento { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder){
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PalestranteEvento>().HasKey(PE => new { PE.EventoId, PE.PalestranteId });
 
-modelBuilder.Entity<PalestranteEvento>().HasKey(PE=> new {PE.EventoId, PE.PalestranteId});
+            modelBuilder.Entity<Evento>().HasMany(e => e.RedesSociais).WithOne(rs => rs.Evento).OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Palestrante>().HasMany(e => e.RedesSociais).WithOne(rs => rs.Palestrante).OnDelete(DeleteBehavior.Cascade);
 
         }
 
